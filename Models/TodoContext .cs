a25730b1e6df1089c24589cebb10fddf5ccc9c74
Clément <clement.llobet@jasparo.fr>
2022-06-21 18:03:10 +0200

    configure database access (work in progress)

---
+++ Models/TodoContext .cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public DbSet<ToDoItem> ToDoItems { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            if (!options.IsConfigured)
+                options.useMySql(Configuration.GetConnectionString("todolist_db"));
+        }
+    }
+}
